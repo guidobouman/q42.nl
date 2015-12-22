5efd0423615bfb3f8f0c321f5cfbe4c270de407c
Guido Bouman <guido@q42.nl>
2015-12-22 18:46:54 +0100

    Trying to get the remote IoT API to work in a collection.

--- to-become-packages/coffeecounter/server/coffeecounter.js
+++ to-become-packages/coffeecounter/server/coffeecounter.js
@@ -1,36 +1,9 @@
 Meteor.startup( () => {
-  if (CoffeeCounter.find().count() === 0){
-    CoffeeCounter.insert({count: 0});
-  }
+  iot_api = DDP.connect('https://iot-api.scalingo.io/');
+  CoffeeCups = new Meteor.Collection('coffeecups', iot_api);
 
-  Meteor.publish("coffeeCounter", () => CoffeeCounter.find());
-
-  updateCoffeeCounter = function() {
-    const date = new Date();
-    const day = date.getDay();
-    const hour = date.getHours();
-    const first = CoffeeCounter.findOne();
-    const currentCount = first.count;
-    let newCount = currentCount;
-
-    // reset at midnight
-    if (hour === 0) {
-      newCount = 0;
-
-    // work day - increment
-    } else if (hour >= 8 && hour <= 18 && day !== 6 && day !== 0){
-      const ceil = hour < 12 ? 20 : 30;
-      const inc = ~~(Math.random() * 42) > ceil ? 1 : 0;
-      newCount = currentCount + inc;
-    }
-
-    if (newCount !== currentCount){
-      CoffeeCounter.update(first._id, {$set: {count: newCount}});
-    }
-
-    // update the counter every minute
-    Meteor.setTimeout(updateCoffeeCounter, 1000 * 60);
-  };
-
-  updateCoffeeCounter();
+  Meteor.publish("coffeeCounter", function() {
+    Counts.publish(this, "coffeeCups", CoffeeCups.find(), { noReady: true })
+    return CoffeeCups.find({}, { limit: 10 })
+  });
 });
